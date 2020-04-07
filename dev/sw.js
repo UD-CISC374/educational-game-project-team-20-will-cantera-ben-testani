48b09942cfc8b1420742956e5bfa2a752da740f6
William Cantera <wcantera@udel.edu>
2020-04-07 01:30:17 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/https://github.com/UD-CISC374/educational-game-project-team-20-will-cantera-ben-testani.git/devprecache-manifest.e1a47b4a78f43ac28825580dd9579f79.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/https://github.com/UD-CISC374/educational-game-project-team-20-will-cantera-ben-testani/devprecache-manifest.0dae471134e6f795298c85e3b6898694.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
