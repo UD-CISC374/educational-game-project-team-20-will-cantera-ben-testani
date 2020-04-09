e3b5939c231fdf1c62fefb60112bc4063fcc25f7
William Cantera <wcantera@udel.edu>
2020-04-08 22:16:01 -0400

    Updates

--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/https://github.com/UD-CISC374/educational-game-project-team-20-will-cantera-ben-testani/alphaprecache-manifest.0b0083e9f2ff7e8ea5f0e9705c4c15a2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-20-will-cantera-ben-testani/alphaprecache-manifest.0a2a074f2e47e337582fc558262a93d6.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
