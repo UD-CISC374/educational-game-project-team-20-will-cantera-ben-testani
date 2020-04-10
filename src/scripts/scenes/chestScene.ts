1359b71cd46138a075699c1986d567582525a013
Benjamin Testani <testanib@udel.edu>
2020-04-10 15:59:47 -0400

    made clock dots draggable

--- src/scripts/scenes/chestScene.ts
+++ src/scripts/scenes/chestScene.ts
@@ -2,6 +2,11 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class ChestScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  closedChest: Phaser.GameObjects.Image;
+  clock: Phaser.GameObjects.Image;
+  hoursDot: Phaser.GameObjects.Image;
+  minuetsDot: Phaser.GameObjects.Image;
+
 
   constructor() {
     super({ key: 'ChestScene' });
@@ -9,10 +14,20 @@ export default class ChestScene extends Phaser.Scene {
 
   create() {
     this.exampleObject = new ExampleObject(this, 0, 0);
-    this.add.image(this.scale.width/2, this.scale.height/2, "closeChest");
-    this.add.image(this.scale.width/2, this.scale.height/2, "clock");
-    this.add.image(this.scale.width/4 + this.scale.width/2, this.scale.height/4 + this.scale.height/2, "hoursDot");
-    this.add.image(this.scale.width/4, this.scale.height/4 + this.scale.height/2, "minutesDot");
+    this.closedChest=this.add.image(this.scale.width/2, this.scale.height/2, "closeChest");
+    this.clock=this.add.image(this.scale.width/2, this.scale.height/2, "clock");
+    this.hoursDot=this.add.image(this.scale.width/4 + this.scale.width/2, this.scale.height/4 + this.scale.height/2, "hoursDot");
+    this.minuetsDot=this.add.image(this.scale.width/4, this.scale.height/4 + this.scale.height/2, "minutesDot");
+
+    this.hoursDot.setInteractive({draggable:true});
+    this.minuetsDot.setInteractive({draggable:true});
+    // http://labs.phaser.io/index.html?dir=input/dragging/&q=
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+
+  });
   }
 
   update() {
